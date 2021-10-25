5135af8d54225263844267e24a373d0e2a997233
EdTheExcited <61635507+EdTheExcited@users.noreply.github.com>
2021-10-25 13:08:38 +0200

    oogabooga

---
+++ Prov1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prov1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write Your Cool Examination Code Here:
+        }
+    }
+}
